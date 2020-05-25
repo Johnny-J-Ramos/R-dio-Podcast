aac869a46d659a31d1b48bd71ce32af0609e2cac
Johnny Jefferson <johnnyjeffersonjrhotmail.com>
2020-05-25 19:43:29 -0300

    foi adicionado alguns elementos

---
+++ js/script.js
@@ -0,0 +1,17 @@
+window.addEventListener('load'start);
+
+var inputRange = null;
+var inputFrequency = null;
+var divPodcasts = null;
+
+function start() {
+  inputRange = document.querySelector('#inputRange');
+  inputFrequency = document.querySelector(onafterprint)
+}
+
+
+
+function start() {
+  console.log('Dom carregado');
+
+}
