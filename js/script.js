a25648e385f7ca1c1c9bb8ab1f2462153522b219
Johnny Jefferson <johnnyjeffersonjrhotmail.com>
2020-05-25 22:14:23 -0300

    atualização dos arquivos

--- js/script.js
+++ js/script.js
@@ -1,15 +1,11 @@
-window.addEventListener('load', start);
+window.addEventListener(`load`, start);
 
 var inputRange = null;
 var inputFrequency = null;
 var divPodcasts = null;
 
 function start() {
-  inputRange = document.querySelector('#inputRange');
-  inputFrequency = document.querySelector('#onafterprint');
+  inputRange = document.querySelector('#inuptRange');
+  inputFrequency = document.querySelector('#inputFrequency');
   divPodcasts = document.querySelector('#divPodcasts');
-
-  inputRange.value = '108';
-  inputFrequency.value = '100';
-  divPodcasts.innerHTML = 'oi';
 }
